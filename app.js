7d79e246de2c4323824f59a5bc436405d6a64c6e
Basia <bsssk3@gmail.com>
2023-01-23 20:00:08 +0100

    shop JS

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// funkcja dodawania na buttonie
-// funkcja odejmowania na buttonie
-const productsArr = [
+
+const shopItemsData = [
     {
         id:1,
         name: "Thor",
         manufacturer: "Empik",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
-        price: 12+"$",
-        image: "/SiiShop/img/img1.jpg",
+        price: 12,
+        image: "./img/img1.jpg",
     },
     {
         id:2,
@@ -18,7 +17,7 @@ const productsArr = [
         image: "./img/img2.jpg",
     },
     {
-        id:2,
+        id:3,
         name: "Gomora",
         manufacturer: "Pop Vinyl",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. balbalbalbala",
@@ -26,7 +25,7 @@ const productsArr = [
         image: "./img/img2.jpg",
     },
     {
-        id:2,
+        id:4,
         name: "Gomora",
         manufacturer: "Pop Vinyl",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
@@ -34,7 +33,7 @@ const productsArr = [
         image: "./img/img2.jpg",
     },
     {
-        id:2,
+        id:5,
         name: "Gomora",
         manufacturer: "Pop Vinyl",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
@@ -42,7 +41,7 @@ const productsArr = [
         image: "./img/img3.jpg",
     },
     {
-        id:2,
+        id:6,
         name: "Gomora",
         manufacturer: "Pop Vinyl",
         description: "",
@@ -50,7 +49,7 @@ const productsArr = [
         image: "./img/img3.jpg",
     },
     {
-        id:2,
+        id:7,
         name: "Gomora",
         manufacturer: "Pop Vinyl",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
@@ -58,7 +57,7 @@ const productsArr = [
         image: "./img/img3.jpg",
     },
     {
-        id:2,
+        id:8,
         name: "Gomora",
         manufacturer: "Pop Vinyl",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
@@ -66,33 +65,95 @@ const productsArr = [
         image: "./img/img3.jpg",
     },
 ]
-console.log(productsArr);
-
-// const prod1 = document.querySelector(".store__offer");
-
-function showProducts(){
-    productsArr.forEach((product) => {
-    document.querySelector(".store__offer").innerHTML +=`<div class="product">       
-            <div class="product__info">
-            <img src="${product.image}" class="product-picture"/>
+let basket = [];
+let cart =[];
+//SHOP PART
+function renderProducts() { //zamienić na forEach
+  
+    shopItemsData.forEach((product) => {
+        let {id, name, image, manufacturer, description, price} = product;
+        document.querySelector(".store__offer").innerHTML +=`<div class="product" id="product-id-${id}">       
+            <div class="product__info" >
+            <img src="${image}" class="product-picture"/>
             <div>
-            <h3 class="product__name">${product.name}</h3>
-            <h4 class="product__manufacturer">${product.manufacturer}</h4>
+            <h3 class="product__name">${name}</h3>
+            <h4 class="product__manufacturer">${manufacturer}</h4>
             </div>
             </div>
-            <p class="product__description">${product.description.substring(0, 70)+"..."}</p>
+            <p class="product__description">${description.substring(0, 70)+"..."}</p>
             
             <div class="product__menu">
-                <div class="centered-item">${product.price}$</div>
-                <div class="centered-item"></div>
+                <div class="centered-item">${price}$</div>
+                <div class="centered-item quantity" id="${id}">0</div>
             <div class="product__buttons">
-                <button class="product__buttons--amount">+</button>
-                <button class="product__buttons--amount">-</button>
+                <button class="product__buttons--quantity" onClick="increment(${id})">+</button>
+                <button class="product__buttons--quantity" onClick="decrement(${id})">-</button>
             </div>
-            <div class="product__buttons--cart-btn centered-item"> <ion-icon name="cart"></ion-icon>
+            <div class="product__buttons--cart-btn centered-item" onClick="addToCart(${id})"> <ion-icon name="cart"></ion-icon>
             </div>
             </div>
             </div>
-        ` })
+            ` })
+        }
+    renderProducts();
+
+    function increment(id) {
+        let search = basket.find((product) =>
+            product.id === id,
+            );
+         
+        if(search===undefined) {
+            const item = shopItemsData.find((product) =>    product.id === id);
+            basket.push({
+            ...item,
+            id: id,
+            numberOfUnits: 1,
+                });
+        } else {
+            search.numberOfUnits += 1;
+        }
+        
+        update(id)
+       
+    };
+
+    function decrement(id){
+        let search = basket.find((product) =>
+        product.id === id);
+
+        if(search.numberOfUnits===0) return;
+        else {
+            search.numberOfUnits -= 1;
+        }
+        update(id)
+    };
+
+    function update(id) {
+        let search = basket.find((product) =>
+        product.id === id);
+        document.getElementById(id).innerHTML = search.numberOfUnits;
+        console.log(basket);
+    };
+
+
+    function addToCart(id) {
+        if (cart.some((item)=> item.id===id)){
+         
+   
+        } else {
+            const item = basket.find((product) =>    product.id === id);
+            cart.push({
+                ...item,
+            })
+            console.log(cart);
+        }
     }
-    showProducts();
+
+
+// CART PART
+
+// function renderCartItems {
+//     cart.forEach((product)=>{
+//         let {manufacturer} = product;
+//     })
+// }
