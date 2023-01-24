15e946281d19b5520ebb3d2be0d4196b9583e151
Barbara Kryszkiewicz <bkryszkiewicz@sii.pl>
2023-01-24 20:38:46 +0100

    all products, styled cart

--- app.js
+++ app.js
@@ -14,63 +14,239 @@ const shopItemsData = [
         name: "Gomora",
         manufacturer: "Pop Vinyl",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
-        price: 12,
+        price: 15,
         image: "./img/img2.jpg",
     },
     {
         id:3,
-        name: "Gomora",
-        manufacturer: "Pop Vinyl",
+        name: "Mighty Thor",
+        manufacturer: "Empik",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. balbalbalbala",
-        price: 12,
-        image: "./img/img2.jpg",
+        price: 20,
+        image: "./img/img3.jpg",
     },
     {
         id:4,
-        name: "Gomora",
-        manufacturer: "Pop Vinyl",
+        name: "Bisky",
+        manufacturer: "PopStore",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
         price: 12,
-        image: "./img/img2.jpg",
+        image: "./img/img4.jpg",
     },
     {
         id:5,
-        name: "Gomora",
-        manufacturer: "Pop Vinyl",
+        name: "Kite",
+        manufacturer: "PopStore",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
         price: 12,
-        image: "./img/img3.jpg",
+        image: "./img/img5.jpg",
     },
     {
         id:6,
-        name: "Gomora",
-        manufacturer: "Pop Vinyl",
-        description: "",
-        price: 12,
-        image: "./img/img3.jpg",
+        name: "Satoru Gojo",
+        manufacturer: "Empik",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. ",
+        price: 15,
+        image: "./img/img6.jpg",
     },
     {
         id:7,
-        name: "Gomora",
-        manufacturer: "Pop Vinyl",
+        name: "Mahito",
+        manufacturer: "Empik",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
         price: 12,
-        image: "./img/img3.jpg",
+        image: "./img/img7.jpg",
     },
     {
         id:8,
-        name: "Gomora",
+        name: "Carmela Soprano",
         manufacturer: "Pop Vinyl",
         description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
         price: 12,
-        image: "./img/img3.jpg",
+        image: "./img/img8.jpg",
+    },
+    {
+        id:9,
+        name: "Cinderella",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 15,
+        image: "./img/img9.jpg",
+    },
+    {
+        id:10,
+        name: "Dark Trooper",
+        manufacturer: "Empik",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 15,
+        image: "./img/img10.jpg",
+    },
+    {
+        id:11,
+        name: "Infinite Deku",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img11.jpg",
+    },
+    {
+        id:12,
+        name: "Care-A-Lot-Bear",
+        manufacturer: "Empik",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img12.jpg",
+    },
+    {
+        id:13,
+        name: "Goblin",
+        manufacturer: "PopStore",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img13.jpg",
+    },
+    {
+        id:14,
+        name: "Marty Bryde",
+        manufacturer: "Empik",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img14.jpg",
+    },
+    {
+        id:15,
+        name: "Wendy Bryde",
+        manufacturer: "Empik",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 15,
+        image: "./img/img15.jpg",
+    },
+    {
+        id:16,
+        name: "Hulk",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 15,
+        image: "./img/img16.jpg",
+    },
+    {
+        id:17,
+        name: "Legolas",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 15,
+        image: "./img/img17.jpg",
+    },
+    {
+        id:18,
+        name: "Zeus",
+        manufacturer: "PopStore",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img18.jpg",
+    },
+    {
+        id:19,
+        name: "Supreme Strange",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 20,
+        image: "./img/img19.jpg",
+    },
+    {
+        id:20,
+        name: "Gohan",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 20,
+        image: "./img/img20.jpg",
+    },
+    {
+        id:21,
+        name: "Olivier Mira",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img21.jpg",
+    },
+    {
+        id:22,
+        name: "Riza Hawkeye",
+        manufacturer: "PopStore",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img22.jpg",
+    },
+    {
+        id:23,
+        name: "Drax",
+        manufacturer: "PopStore",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 20,
+        image: "./img/img23.jpg",
+    },
+    {
+        id:24,
+        name: "Mantis",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img24.jpg",
+    },
+    {
+        id:25,
+        name: "Fawkes",
+        manufacturer: "PopStore",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 15,
+        image: "./img/img25.jpg",
+    },
+    {
+        id:26,
+        name: "Hermione Granger",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img26.jpg",
+    },
+    {
+        id:27,
+        name: "Neville Longbottom",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img27.jpg",
+    },
+    {
+        id:28,
+        name: "Ron Weasley",
+        manufacturer: "PopStore",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img28.jpg",
+    },
+    {
+        id:29,
+        name: "Hawkeye",
+        manufacturer: "Pop Vinyl",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img29.jpg",
+    },
+    {
+        id:30,
+        name: "Maya Lopez",
+        manufacturer: "PopStore",
+        description: "Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall. Comes with a clear, built-in stand. Vinyl bobblehead is 11.6 cm tall.",
+        price: 12,
+        image: "./img/img30.jpg",
     },
 ];
 let basket = [];
 let cart =[];
 // DOM
 let storeOffer = document.querySelector(".store__offer");
-let cartOffer = document.querySelector(".cart__offer");
+let cartOffer = document.querySelector(".incart__offer--container");
 
 //SHOP PART
 function renderProducts() { 
@@ -79,12 +255,12 @@ function renderProducts() {
         storeOffer.innerHTML +=`<div class="product" id="product-id-${id}">       
             <div class="product__info" >
             <img src="${image}" class="product-picture"/>
-            <div>
+            <div class="product__data">
             <h3 class="product__name">${name}</h3>
             <h4 class="product__manufacturer">${manufacturer}</h4>
             </div>
             </div>
-            <p class="product__description">${description.substring(0, 70)+"..."}</p>
+            <p class="product__description">${description.substring(0, 70)}</p>
             
             <div class="product__menu">
                 <div class="centered-item">${price}$</div>
@@ -164,24 +340,27 @@ renderProducts();
     function renderCartItems(id) {
     cart.forEach((product) => {
         let {name, manufacturer, price, numberOfUnits} = product;
-        cartOffer.innerHTML += `<div class="cart__offer--container">
+        cartOffer.innerHTML += `  <div class="incart__manu">
+        <div class="incart">
+            <input type="checkbox"/>
+            <h4 class="incart__manufacturer">${manufacturer}</h4>
+            </div>
+        <div class="incart__grid">
+        <div class="incart__product">
         <input type="checkbox"/>
-        <div>${manufacturer}</div>
-    <div class="cart_product">
-    <input type="checkbox"/>
-    <div class="product__name">${name}</div>
-    <div class="product__price">${price}</div>
-    <div class="quantity">${numberOfUnits}</div>
-    <div class="product__buttons">
+        <div class="incart__name">${name}</div>
+        <div class="incart__price">${price}$</div>
+        <div class="incart__quantity">${numberOfUnits}</div>
+        <div class="incart__buttons">
         <button class="product__buttons--quantity" onClick="increment()">+</button>
         <button class="product__buttons--quantity" onClick="decrement()">-</button>
+        </div>
     </div>
-<div class="cart__delete">obrazek</div>
-</div>
-
-<div class="cart_offer--footer"></div>
-
-</div>
+    <div class="incart__delete"><ion-icon name="trash-outline"></ion-icon></div>
+    
+    </div>
+    </div>
+    <div class="incart__footer">Total: </div>
 `})
 }
 
